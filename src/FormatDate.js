c4edffa184322fc1576b3af6ed6e8ab02b779ebb
elliebanthony <elliebanthony@gmail.com>
2022-04-22 17:11:17 -0400

    trying to fix bug in format

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -10,19 +10,18 @@ export default function FormatDate(props) {
     "Friday",
     "Saturday",
   ];
-  let day = days[props.date.getDay()];
+  let day = days(props.date.getDay());
   let hours = props.date.getHours();
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0 ${minutes}`;
-  } else {
-    return (
-      <div>
-        <h3>
-          {" "}
-          {day}, {hours}:{minutes}
-        </h3>
-      </div>
-    );
   }
+  return (
+    <div>
+      <h3>
+        {" "}
+        {day}, {hours}:{minutes}
+      </h3>
+    </div>
+  );
 }
