4af0bef844be8e07b85b6a8fa3178f8f2399e267
elliebanthony <elliebanthony@gmail.com>
2022-04-28 13:21:16 -0400

    added axios for forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,6 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 export default function Forecast() {
+  function displayForecast(response) {
+    console.log(response.data);
+  }
+  let apiKey = "61585f15453918f9f78604040a26d7b6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=74&lon=40.7&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+
   return (
     <div className="row">
       <div className="col-2">
