ed2ca9b0f72c4abdec074bf6df7a51132b37aed2
elliebanthony <elliebanthony@gmail.com>
2022-04-28 13:05:27 -0400

    adding html for forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function Forecast(props) {
-  return <div></div>;
+export default function Forecast() {
+  return (
+    <div className="row">
+      <div className="col-2">
+        <div className="day">Friday</div>
+        <img
+          src="https://openweathermap.org/img/wn/10d@2x.png"
+          alt="Clear"
+          id="icon"
+        />
+        <div className="highLow">
+          {" "}
+          <span> H: 10°C </span>
+          <span> L: 5°C </span>
+        </div>
+      </div>
+    </div>
+  );
 }
