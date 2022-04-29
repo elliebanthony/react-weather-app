0c54a0717f2f96092b490be09c4017cc0fd904f1
elliebanthony <elliebanthony@gmail.com>
2022-04-29 14:44:10 -0400

    adding functions for day and min and max

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 export default function ForecastDay(props) {
+  function maxTemp() {
+    let max = Math.round(props.data.temp.max);
+    return ` H:${max} °C`;
+  }
+  function minTemp() {
+    let min = Math.round(props.data.temp.min);
+    return ` L:${min} °C`;
+  }
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "THur", "Fri", "Sat"];
+
+    return days[day];
+  }
   return (
     <div className="row">
       <div className="col-2">
-        <div className="day">{props.data.dt} </div>
+        <div className="day"> {day()} </div>
         <img src={props.data.weather[0].icon} alt="Clear" id="icon" />
         <div className="highLow">
           {" "}
-          <span> H: {Math.round(props.data.temp.max)} °C</span>
-          <span> L: {Math.round(props.data.temp.min)}°C </span>
+          <span> {maxTemp()}</span>
+          <span> {minTemp()}</span>
         </div>
       </div>
     </div>
